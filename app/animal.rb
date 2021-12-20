12169847f1447b3b805bf449ebbf50380f7e43ae
Yulia Lime <lime666lime@gmail.com>
2021-12-20 22:43:56 +0200

    deleted some methods

--- app/animal.rb
+++ app/animal.rb
@@ -18,8 +18,6 @@ class Animal
         r.set_cookie('energy', @energy)
         r.set_cookie('asleep', @asleep)
         r.set_cookie('lives', @lives)
-        r.set_cookie('stuff_in_belly', @stuff_in_belly)
-        r.set_cookie('stuff_in_intestine', @stuff_in_intestine)
         r.set_cookie('birth_time', @birth_time)
         r.set_cookie('name', @request.params['name'])
         r.redirect('/play')
@@ -28,9 +26,8 @@ class Animal
     when '/play'
       return Pet_Methods.rave_params(@request, 'rave') if @request.params['rave']
       return Pet_Methods.sleep_params(@request, 'sleep') if @request.params['sleep']
-      return Pet_Methods.change_params(@request, 'eat') if @request.params['eat']
+      return Pet_Methods.eat_params(@request, 'eat') if @request.params['eat']
       return Pet_Methods.bath_params(@request, 'bath') if @request.params['bath']
-      return Pet_Methods.change_params(@request, 'status') if @request.params['status']
       return Pet_Methods.pet_params(@request, 'pet') if @request.params['pet']
       Rack::Response.new(render('play.html.erb'))
     when '/exit'
@@ -62,73 +59,7 @@ class Animal
     @lives = 10
     @energy = 10
     @birth_time = Time.new
-    $COMMANDS = %w[eat rest rave wakeup pet bath wc voice help status age exit]
-  end
-
-  def eat
-    if @stuff_in_belly <= 5 && @energy <= 5
-      sleep 2
-      p 'Спасибо, было очень вкусно, хотя я хз чем ты меня накормил.'
-      p 'Теперь хочу тусить.'
-      @energy += 2
-      @stuff_in_belly += 3
-      @stuff_in_intestine += 2
-    else
-      p 'Спасибо, я не голоден.'
-    end
-  end
-
-  def sleep
-    p 'Вы укладываете животину спать.'
-    @asleep = true
-    @energy += 3
-    time_to_pass
-  end
-
-  def rave
-    p "Вы играете с #{@name}."
-    @energy -= 2
-    p 'Жизнь меня к такому не готовила, хозяин.' if @energy <= 6
-    time_to_pass
-    p 'Не любишь ты меня, хозяин. Помер я.' if death
-  end
-
-  def pet
-    p "Вы чешите #{@name} пузико."
-    @energy -= 1
-    p 'ур-ур-ур '
-    sleep 2
-    p 'Спасибушки, хозяин. Я бы тоже хотел тебя почесать =)'
-    time_to_pass
-  end
-
-  def bath
-    p "#{@name} купется."
-    @energy -= 1
-    time_to_pass
-  end
-
-  def wc
-    if @stuff_in_intestine >= 7
-      p 'Жизнь обрела краски.'
-      @stuff_in_intestine = 0
-    else
-      p 'Чуть позже, пожалуйста.'
-    end
-  end
-
-  def status
-    p 'Зачем я живу? Даже покормить некому...' if hungry?
-    p 'Где упал, там и кровать.' if want_to_sleep?
-    p '...' if death
-    p "#{@lives} - осталось жизней."\
-      "#{@energy} - уровень бодрости."\
-      "#{@stuff_in_belly} - уровень еды внутри."\
-      "#{@stuff_in_intestine} - решайте сами вести в уборную или нет."
-  end
-
-  def age
-    pet_age
+    $COMMANDS = %w[rave rest eat pet age exit bath]
   end
 
   private
--- app/lib/pet_methods.rb
+++ app/lib/pet_methods.rb
@@ -1,4 +1,5 @@
 module Pet_Methods
+=begin
   def self.change_params(request, name)
 
     Rack::Response.new do |r|
@@ -11,11 +12,28 @@ module Pet_Methods
       r.redirect('/play')
     end
   end
+=end
+  def self.eat_params(request, name)
+
+    Rack::Response.new do |r|
+      r.set_cookie(name, request.cookies["#{name}"].to_i + 2)
+
+      command = ($COMMANDS - [name]).last
+
+      r.set_cookie(command, request.cookies["#{command}"].to_i - 1)
+
+      r.redirect('/play')
+    end
+  end
 
   def self.rave_params(request, name)
 
     Rack::Response.new do |r|
       r.set_cookie(name, request.cookies["#{name}"].to_i - 3)
+
+      command = ($COMMANDS - [name]).last
+
+      r.set_cookie(command, request.cookies["#{command}"].to_i - 2)
 
       r.redirect('/play')
     end
@@ -26,6 +44,10 @@ module Pet_Methods
     Rack::Response.new do |r|
       r.set_cookie(name, request.cookies["#{name}"].to_i + 2)
 
+      command = ($COMMANDS - [name]).last
+
+      r.set_cookie(command, request.cookies["#{command}"].to_i - 1)
+
       r.redirect('/play')
     end
   end
@@ -35,6 +57,10 @@ module Pet_Methods
     Rack::Response.new do |r|
       r.set_cookie(name, request.cookies["#{name}"].to_i + 3)
 
+      command = ($COMMANDS - [name]).first
+
+      r.set_cookie(command, request.cookies["#{command}"].to_i + 2)
+
       r.redirect('/play')
     end
   end
@@ -43,6 +69,10 @@ module Pet_Methods
 
     Rack::Response.new do |r|
       r.set_cookie(name, request.cookies["#{name}"].to_i + 4)
+
+      command = ($COMMANDS - [name]).first
+
+      r.set_cookie(command, request.cookies["#{command}"].to_i + 1)
 
       r.redirect('/play')
     end
